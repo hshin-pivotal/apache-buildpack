e01069c0944be53d60319e181a32ed956418de70
Shaozhen Ding <dsz0111@gmail.com>
2014-07-07 17:26:00 -0400

    Create apache buildpack

--- bin/boot.sh
+++ bin/boot.sh
@@ -1,3 +1,6 @@
-export APP_ROOT=$HOME
+export APP_ROOT=/home/vcap/app
 
-exec $APP_ROOT/bin/apachectl start
+mv $APP_ROOT/httpd.conf $APP_ROOT/httpd.conf.org
+erb $APP_ROOT/httpd.conf.org > $APP_ROOT/httpd.conf
+(tail $APP_ROOT/httpd/logs/*log &)
+exec $APP_ROOT/httpd/bin/httpd -f $APP_ROOT/httpd.conf -DFOREGROUND
