0e1a422713ac8b78324f5c9caa8c0bc034518f1b
Haewon Shin <hshin@pivotal.io>
2019-06-05 23:46:16 +0900

    Update boot.sh

--- bin/boot.sh
+++ bin/boot.sh
@@ -1,6 +1,8 @@
+export APP_ROOT=/home/vcap/app
 export APACHE_ROOT=/home/vcap/app/httpd
 
 mv $APACHE_ROOT/conf/httpd.conf $APACHE_ROOT/conf/httpd.conf.org
 erb $APACHE_ROOT/conf/httpd.conf.org > $APACHE_ROOT/conf/httpd.conf
+mv $APP_ROOT/custom/htdocs $APACHE_ROOT
 (tail $APACHE_ROOT/logs/*log &)
 exec $APACHE_ROOT/bin/httpd -DFOREGROUND
